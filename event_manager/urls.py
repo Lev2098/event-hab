d055a066a9c207d95d02d42c2c8349fefa978a8b
lev2099 <lev2099@gmail.com>
2024-11-20 12:28:16 +0200

    add urls -user detail -user update -event feedback -event participate -registration

--- event_manager/urls.py
+++ event_manager/urls.py
@@ -5,14 +5,25 @@ from .views import (index,
                     EventDetailView,
                     EventCreateView,
                     EventUpdateView,
-                    EventDeleteView, UserListView,
-
+                    EventDeleteView,
+                    UserListView,
+                    register,
+                    UserDetailView,
+                    UserUpdateView,
+                    add_feedback,
+                    participate_in_event,
                     )
 
 
 urlpatterns = [
     path("", index, name="index"),
+
     path("users/", UserListView.as_view(), name="user-list"),
+    path("user/<int:pk>/detail/", UserDetailView.as_view(), name="user-detail"),
+    path('user/<int:pk>/update/', UserUpdateView.as_view(), name='user-update'),
+
+    path('event/<int:pk>/feedback/', add_feedback, name='add-feedback'),
+    path('event/<int:pk>/participate/', participate_in_event, name='participate-in-event'),
     path("events/", EventListView.as_view(), name="event-list"),
     path("event/<int:pk>/", EventDetailView.as_view(), name="event-detail"),
     path(
@@ -29,7 +40,9 @@ urlpatterns = [
         "event/<int:pk>/delete/",
         EventDeleteView.as_view(),
         name="event-delete"
-    )
+    ),
+
+    path("registration/", register, name="registration"),
 ]
 
 app_name = "event-manager"
